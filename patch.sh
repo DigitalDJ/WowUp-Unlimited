b7c682b6772cc1b4172107f8821a477204b341fc
Grant Pannell <300992+DigitalDJ@users.noreply.github.com>
2024-08-29 02:12:37 +0930

    Rebase against WowUp v2.20.0

--- patch.sh
+++ patch.sh
@@ -6,16 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 GITHUB_WORKSPACE="${GITHUB_WORKSPACE:=${SCRIPT_DIR}/../}"
 
 CURR_REPO_DIR="${GITHUB_WORKSPACE}/WowUp"
-MERGE_REPO_DIR="${GITHUB_WORKSPACE}/WowUp.CF"
-
-while IFS= read -r f ; do
-  d=$(dirname "${f}")
-  if [ ! -d "${CURR_REPO_DIR}/${d}" ] ; then
-    mkdir -p "${CURR_REPO_DIR}/${d}"
-  fi
-  echo "${f}"
-  cp -af "${MERGE_REPO_DIR}/${f}" "${CURR_REPO_DIR}/${f}"
-done < "${SCRIPT_DIR}/unlimited-patches/orphans.lst"
 
 find "${SCRIPT_DIR}/unlimited-patches/" -name "*.patch" -type f -print | sort -n |
 while IFS= read -r f ; do
